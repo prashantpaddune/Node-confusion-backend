f796123c8f1c5eadc8e27db8d687cfb684f5ec3c
prashantpaddune <elonpras@my.smccd.edu>
2020-08-03 11:23:39 +0530

    Express Router

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
-app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
 
 app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-
-    res.send('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
